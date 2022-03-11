fcbf04957bbc21e3379f2d94d58ca80847df8cb4
EllVodka <eric.sergueev@gmail.com>
2022-03-11 17:27:12 +0100

    ajout des bindParam manquant

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ include "connexion.php";
 
 $requete = $pdo->prepare("SELECT praticien.nom , praticien.prenom , praticien.id_ville, type_praticien.libelle AS specialite FROM praticien LEFT JOIN type_praticien ON praticien.code_type_praticien = type_praticien.code LEFT JOIN ville ON praticien.id_ville = ville.id INNER JOIN departement ON ville.id_departement = departement.id WHERE praticien.id_ville LIKE '%:ville%' AND praticien.code_type_praticien LIKE '%:code%' AND departement.id LIKE '%:departement%';");
 $requete->bindParam(':ville',$_POST["id_ville"]);
+$requete->bindParam(':code',$_POST["code_type_praticien"]);
+$requete->bindParam(':departement',$_POST["id"]);
 $requete->execute();
 
 $resultats = $requete->fetchAll();
