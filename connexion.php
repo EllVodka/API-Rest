c531a88c43aeec530d2b863fc5dacfd42e94c31c
EllVodka <eric.sergueev@gmail.com>
2022-03-11 23:58:59 +0100

    ajout de l'api fonctionnel

--- connexion.php
+++ connexion.php
@@ -1,8 +1,9 @@
 <?php
+
+header('Content-Type: application/json');
+
 try{
 	$pdo = new PDO('mysql:host=localhost;dbname=gsb_practicien',"root","");
-	$retour["success"] = true;
-	$retour["message"] = "Connexion a la base de données reussite";
 }
 catch (Exception $e) {
 	$retour["success"] = false;
--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <?php
-header('Content-Type: application/json');
+include "template.php";
 
-include "connexion.php";
+//Création d'une parti de la requete preparé
+$sql = "SELECT praticien.nom , praticien.prenom , praticien.id_ville, type_praticien.libelle AS specialite FROM praticien LEFT JOIN type_praticien ON praticien.code_type_praticien = type_praticien.code LEFT JOIN ville ON praticien.id_ville = ville.id INNER JOIN departement ON ville.id_departement = departement.id";
 
-$requete = $pdo->prepare("SELECT praticien.nom , praticien.prenom , praticien.id_ville, type_praticien.libelle AS specialite FROM praticien LEFT JOIN type_praticien ON praticien.code_type_praticien = type_praticien.code LEFT JOIN ville ON praticien.id_ville = ville.id INNER JOIN departement ON ville.id_departement = departement.id WHERE praticien.id_ville LIKE '%:ville%' AND praticien.code_type_praticien LIKE '%:code%' AND departement.id LIKE '%:departement%';");
-$requete->bindParam(':ville',$_POST["id_ville"]);
-$requete->bindParam(':code',$_POST["code_type_praticien"]);
-$requete->bindParam(':departement',$_POST["id"]);
-$requete->execute();
+if(isset($_POST['ville']) && isset($_POST['code']) && isset($_POST['departement']))
+{
+    //Si le client a saisie la ville, le code praticien et le departement, on filtre les données via MYSQL
+    $sql .= " WHERE praticien.id_ville LIKE :ville AND praticien.code_type_praticien LIKE :code AND departement.id LIKE :departement;";
+    $requete = $pdo->prepare($sql);
 
-$resultats = $requete->fetchAll();
+    //ajout des paramètre 
+    $requete->bindParam(':ville',$_POST["ville"]);
+    $requete->bindParam(':code',$_POST["code"]);
+    $requete->bindParam(':departement',$_POST["departement"]);    
+}
+else
+{
+    //fin de la requete
+    $sql .= ";";
+    $requete = $pdo->prepare($sql);
+}
 
-$retour["success"] = true;
-$retour["message"] = "Voici les infos sur les praticien";
-$retour["results"]["nb"] = count($resultats);
-$retour["results"]["praticien"] = $resultats;
+//Execution de la requete
+if($requete->execute()){
+    $resultats = $requete->fetchAll();
+
+    $success = true;
+    $data["nombre"] = count($resultats);
+    $data["praticien"] = $resultats;
+    $msg = " Voici les infos sur les praticien";
+}else
+{
+    $msg = "Une erreur s'est produite";
+}
 
 
-echo json_encode($retour);
 
+reponse_json($success,$data,$msg)
 ?>
